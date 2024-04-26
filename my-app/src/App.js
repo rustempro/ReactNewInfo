a301ac3994fe1d7694dd68866d53e474e694e4c5
rustempro <rustemmustafin@yandex.ru>
2024-04-26 09:29:57 +0300

    res 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 export const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload 2
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+	const year = new Date().getFullYear();
+
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+
+			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to reload',
+			),
+
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+
+			createElement('div', null, year),
+		),
 	);
 };
